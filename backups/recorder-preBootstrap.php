fa854b68632e583ed1aa990ee4f614c982839cb7
dimitri-prica <55683056+dimitri-prica@users.noreply.github.com>
2019-10-25 05:04:19 -0700

    Add files via upload

--- backups/recorder-preBootstrap.php
+++ backups/recorder-preBootstrap.php
@@ -1,7 +1,7 @@
 <DOCTYPE html>
 <html><head>
-<link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
-<script src="/bootstrap/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="/var/www/html/bootstrap/css/boostrap.min.css">
+<script src="/var/www/html/bootstrap/js/boostrap.min.js"></script>
 <meta http-equiv="content-type" content="text/html; charset=windows-1252">
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 	<title>Audio Recorder</title>
@@ -11,7 +11,6 @@
         session_start();
         $user_id = $_SESSION["user_id"];
         echo htmlspecialchars($user_id);
-        $reading_passage = file_get_contents('language/eng/reading_passage.txt');
     ?>
     </div>
     <script>
@@ -22,7 +21,6 @@
 	<script src="js/audiodisplay.js"></script>
 	<script src="js/recorderjs/recorder.js"></script>
 	<script src="js/main.js"></script>
-
 	<style>
 	html { overflow: hidden; }
 	body { 
@@ -72,7 +70,7 @@
 		background: radial-gradient(center, ellipse cover, #ff0000 0%,lightgrey 75%,lightgrey 100%,#7db9e8 100%); 
 	}
 
-
+<?php $reading_passage = file_get_contents('language/eng/reading_passage.txt'); ?>
 
 .column-container {display: flex; width: 100%; height: 100%; flex-direction: row;}
 .second-column { display:flex; flex-direction: column; width: 30%; min-width: 300px; overflow: auto; }
@@ -113,57 +111,42 @@ table {  font-family: arial, sans-serif;  border-collapse: collapse;  width: 100
 	</style>
 </head>
 <body>
-	<nav class="navbar navbar-primary bg-primary">
-        <span class="navbar-brand mb-0 h1">Recorder Tool</span>
-    </nav>
 	<div class="column-container">
 	<div class="first-column">
-		<div class="accordion" id="accordionExample">
-  <div class="card">
-    <div class="card-header" id="headingOne">
-      <h2 class="mb-0">
-        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-          Instructions
-        </button>
-      </h2>
-    </div>
-
-    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-      <div class="card-body">
-        You can see the recording pane on the right-hand side. The top Recording window shows the level of the incoming sound. The bottom Recording window will show the recorded waveform when it is complete. Underneath the two windows are a Playback Control, a Microphone icon, and a Submit icon (an arrow going into a hard drive). To record the story, click on the Microphone icon (which will glow red), read the passage aloud, and then click on the Microphone icon again to stop recording. The lower window will then show your recording as a waveform. Clicking on the Play button in the Playback Control will allow you to hear your recording. If you are satisfied with it, you can click on the Submit button and your recording will be sent to the University of British Columbia. Once you have submitted your recording, you canclick on Close Recording Pane to make the Recording Pane go away.
-      </div>
-    </div>
+		<button type="button" class="collapse">Hide Instructions</button>
+		<div id="instructions" class="content">
+    You can see the recording pane on the right-hand side. The top Recording window shows the
+level of the incoming sound. The bottom Recording window will show the recorded waveform
+when it is complete. Underneath the two windows are a Playback Control, a Microphone icon,
+and a Submit icon (an arrow going into a hard drive). To record the story, click on the
+Microphone icon (which will glow red), read the passage aloud, and then click on the
+Microphone icon again to stop recording. The lower window will then show your recording as a
+waveform. Clicking on the Play button in the Playback Control will allow you to hear your
+recording. If you are satisfied with it, you can click on the Submit button and your recording will
+be sent to the University of British Columbia. Once you have submitted your recording, you can
+click on Close Recording Pane to make the Recording Pane go away.
   </div>
-  <div class="card">
-    <div class="card-header" id="headingTwo">
-      <h2 class="mb-0">
-        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-          Reading Sample: The Benefits of Hiking
-        </button>
-      </h2>
-    </div>
-    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-      <div class="card-body">
-        <?php echo $reading_passage ?>
-      </div>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-header" id="headingThree">
-      <h2 class="mb-0">
-        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-          SPontaneous Speech Prompts
-        </button>
-      </h2>
-    </div>
-    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-      <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-</div>
+  	<div>
+		<h1>Reading Sample: The Benefits of Hiking</h1>
+		<p><?php echo $reading_passage ?></p>
+		</div>
 	</div>
+<script>
+var coll = document.getElementsByClassName("collapse");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "none") {
+      content.style.display = "block";
+    } else {
+      content.style.display = "none";
+    }
+  });
+}
+</script>
 
 	<div class="second-column">
 	<div id="viz">
